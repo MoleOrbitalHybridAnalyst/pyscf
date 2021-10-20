a9943dea58d0627158da0b25ce1dc075529ad53d
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2021-10-19 18:36:11 -0700

    add preliminary dual basis hartree fock

---
+++ pyscf/scf/dualbase_hf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+#
+# Author: Chenghan Li <lch004218@gmail.com>
+#
+
+'''
+Dual Basis Hartree-Fock
+'''
+
+from pyscf.scf import hf
+
+class DualBaseRHF(hf.RHF):
+
+   def __init__(self, mol, mol2):
+      '''
+      I expect mol and mol2 only differ by basis set, but I do not check!
+      '''
+      super(hf.RHF, self).__init__(mol)
+
+      if not mol2._built:
+         sys.stderr.write('Warning: %s must be initialized before calling SCF.\n'
+                           'Initialize %s in %s\n' % (mol2, mol2, self))
+         mol2.build()
+
+      self.mol2 = mol2
+
+
+   def kernel(self, conv_tol=1e-10, conv_tol_grad=None,
+         dump_chk=True, dm0=None, callback=None, conv_check=True, **kwargs):
+      '''
+      Examples:
+
+      >>> from pyscf import gto, scf
+      >>> mol = gto.M(atom='H 0 0 0; H 0 0 1.1', basis='cc-pvdz')
+      >>> mol2 = gto.M(atom='H 0 0 0; H 0 0 1.1', basis='cc-pvtz')
+      >>> mf = scf.dualbase_hf.DualBaseRHF(mol, mol2)
+      >>> conv, e, mo_e, mo, mo_occ = mf.kernel()
+      >>> print('conv = %s, E(HF) = %.12f' % (conv, e))
+      '''
+
+      # SCF using smaller basis in mol1
+      conv, e, mo_e, mo, mo_occ = hf.kernel(self, conv_tol, conv_tol_grad, 
+            dump_chk, dm0, callback, conv_check, **kwargs)
+      logger.info(mf, 'SCF on small basis E= %.15g', e)
+
+      # project dm_small into dm_proj using larger basis in mol2
+      dm_small = self.make_rdm1(mo, mo_occ)
+      dm_proj = addons.project_dm_nr2nr(self.mol, dm_small, self.mol2)
+
+      # construct fock from dm_proj
+      h1e = self.get_hcore(mol2)
+      vhf = self.get_veff(mol2, dm_proj)
+      fock = h1e + vhf
+
+      # diagonalization once
+      s1e = self.get_ovlp(mol2)
+      mo_energy, mo_coeff = self.eig(fock, s1e)
+      mo_occ = mf.get_occ(mo_energy, mo_coeff)
+
+      # update energy
+      dm_large = self.make_rdm1(mo_coeff, mo_occ)
+      e_tot = self.energy_tot(dm_large, h1e)
+
+      return conv, e_tot, mo_energy, mo_coeff, mo_occ
