b31a8913bcdba1a319fc85e6f498448bd693627d
MoleOrbitalHybridAnalyst <lch004218@gmail.com>
2022-05-25 14:47:19 -0700

    add example under fci/cpci

--- pyscf/fci/cpci.py
+++ pyscf/fci/cpci.py
@@ -5,7 +5,7 @@ import scipy.sparse.linalg as sla
 import numpy as np
 
 def solve(h1e, eri, ECI, ci, dHc, norb, nelec, 
-        dci0=None, tol=1e-6, max_cycle=20):
+        dci0=None, tol=1e-9, max_cycle=20):
     '''
     solve for dc^CI / dR from 
     (H^CI - E^CI + 2 c^CI \outer c^CI) dc^CI / dR = dHc
@@ -44,3 +44,58 @@ def solve(h1e, eri, ECI, ci, dHc, norb, nelec,
 
     return sol.reshape((Na,Nb)), stat
 
+if __name__ == "__main__":
+    np.random.seed(1)
+
+    from pyscf import gto, scf, fci, ao2mo
+    mol = gto.Mole()
+    mol.atom = '''
+    O 0 0      0
+    H 0 1.0000 0.000
+    H 0 -0.5 0.866025404'''
+    mol.basis = 'sto-3g'
+    mol.build()
+    nao = mol.nao
+    nelec = mol.nelectron
+
+    mf = scf.RHF(mol)
+    mf.kernel()
+
+    h1 = mf.mo_coeff.T @ mf.get_hcore() @ mf.mo_coeff
+    h2 = ao2mo.full(mf._eri, mf.mo_coeff, nao)
+
+    solver = fci.direct_spin1.FCI()
+    solver.conv_tol = 1e-12
+    E, c = solver.kernel(h1, h2, nao, nelec)
+
+    dh1 = np.random.random(h1.shape)
+    dh1 = 1e-4 * dh1 / np.linalg.norm(h1)
+    dh1 = (dh1 + dh1.T) / 2
+    dh2 = np.random.random(h2.shape)
+    dh2 = 1e-4 * dh2 / np.linalg.norm(h2)
+    dh2 = (dh2 + dh2.T) / 2
+
+    E1, c1 = solver.kernel(h1+dh1/2, h2+dh2/2, nao, nelec)
+    E2, c2 = solver.kernel(h1-dh1/2, h2-dh2/2, nao, nelec)
+    num_dc = c1-c2
+
+    def Hc(h1, h2, c):
+        h2e = fci.direct_spin1.absorb_h1e(h1, h2, nao, nelec, 0.5)
+        return fci.direct_spin1.contract_2e(h2e, c, nao, nelec)
+
+    dHc = (E1-E2) * c - Hc(dh1, dh2, c)
+    dc, stat = solve(h1, h2, E, c, dHc, nao, nelec, tol=1e-12)
+    assert stat == 0
+
+    # check gmres via (H-E) dc == dHc
+    Hdc = Hc(h1, h2, dc) - E*dc
+    assert np.linalg.norm(Hdc-dHc) / np.linalg.norm(dHc) < 1e-7
+
+    # check CPCI eq via (H-E) num_dc == dHc
+    Hndc = Hc(h1, h2, num_dc) - E*num_dc
+    assert np.linalg.norm(Hndc-dHc) < 1e-7
+
+    # check identity (H2-E2) num_dc == (num_dE-num_dH) c1
+    H2ndc = Hc(h1-dh1/2, h2-dh2/2, num_dc) - E2*num_dc
+    ndHc1 = -Hc(dh1, dh2, c1) + (E1-E2)*c1
+    assert np.linalg.norm(H2ndc-ndHc1) < 1e-7
